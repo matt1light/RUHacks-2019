507f0245aa6d6aa8d1dec0bf1b4a8ca5fd979ae7
Julia <julia.st.jean@winwinhomesharing.com>
2019-05-18 19:37:30 -0400

    More Match stuff

--- client/src/components/MatchCards.js
+++ client/src/components/MatchCards.js
@@ -1,64 +1,83 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import React, {Component} from 'react';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
 
-const styles = {
-    card: {
-        maxWidth: 345,
-    },
-    media: {
-        // ⚠️ object-fit is not supported by IE 11.
-        objectFit: 'contain',
-    },
-};
-
-function MatchCard(props) {
-    const { classes } = props;
-    return (
-        <div>
-        <h1>Matches</h1>
-        <Card className={classes.card}>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    alt="Contemplative Reptile"
-                    className={classes.media}
-                    height="140"
-                    image="https://randomuser.me/api/portraits/men/70.jpg"
-                    title="Contemplative Reptile"
-                />
-                <CardContent>
-                    <h2> first_name last_name</h2>
-                    <p> Age: <br/>
-                        City: <br/>
-                        Closest School: <br/>
-                        Tasks: <br/>
-                        Match: % <br/>
-                    </p>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Share
-                </Button>
-                <Button size="small" color="primary">
-                    Learn More
-                </Button>
-            </CardActions>
-        </Card>
-        </div>
-    );
+const cardStyle = {
+    'borderStyle': 'dashed',
+    'borderColor': 'rgba(0,0,0,0.3)',
 }
 
-MatchCard.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
 
-export default withStyles(styles)(MatchCard);
+class MatchCard extends Component{
+    clickMatch= () =>{
+        const button = document.getElementById('matchButton')
+        button.innerText= 'Matched :)'
+    };
+
+    render(){
+        return(
+            <div>
+                <h1>Matches</h1>
+                <Grid
+                    container
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="center"
+                    spacing = {40}
+                >
+
+                <Card style={cardStyle}>
+                    <CardMedia
+                        component="img"
+                        alt="Avatar"
+                        height="140"
+                        image="https://randomuser.me/api/portraits/men/50.jpg"
+                        title="Avatar"
+                        style = {{'objectFit': 'contain'}}
+                    />
+                    <CardContent>
+                        <h2> first_name last_name</h2>
+                        <p> Age: <br/>
+                            City: <br/>
+                            Closest School: <br/>
+                            Tasks: <br/>
+                            Match: % <br/>
+                        </p>
+                    </CardContent>
+                    <div align = 'center'>
+                        <button type="button" id ='matchButton' onClick = {this.clickMatch}>Match!</button>
+                    </div>
+                </Card>
+                    <Card style={cardStyle}>
+                        <CardMedia
+                            component="img"
+                            alt="Avatar"
+                            height="140"
+                            image="https://randomuser.me/api/portraits/men/50.jpg"
+                            title="Avatar"
+                            style = {{'objectFit': 'contain'}}
+                        />
+                        <CardContent>
+                            <h2> first_name last_name</h2>
+                            <p> Age: <br/>
+                                City: <br/>
+                                Closest School: <br/>
+                                Tasks: <br/>
+                                Match: % <br/>
+                            </p>
+                        </CardContent>
+                        <div align = 'center'>
+                            <button type="button" id ='matchButton' onClick = {this.clickMatch}>Match!</button>
+                        </div>
+                    </Card>
+                </Grid>
+            </div>
+
+        );
+    }
+}
+
+
+export default MatchCard;
