e7eea216d0715164c1a92fd0f6c5ed9ed70d4bcb
matt1light <matt1light@gmail.com>
2019-05-18 12:07:18 -0400

    Update store with realtime changes to input form
    Only handles name, age, and school
    No validation yet

--- client/src/components/IntakeFormPage.js
+++ client/src/components/IntakeFormPage.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { updateForm } from '../actions/intake';
 
 export class IntakeFormPage extends Component {
-    state = {  }
+    state = {}
+    onSubmit = (e) => {
+        e.preventDefault();
+        // console.log(e);
+    }
+    onNameChange = (e) => {
+        this.props.dispatch(updateForm({name: e.target.value}))
+    }
+    onAgeChange = (e) => {
+        this.props.dispatch(updateForm({age: e.target.value}))
+    }
+    onSchoolChange = (e) => {
+        this.props.dispatch(updateForm({school: e.target.value}))
+    }
     render() { 
         return (
-        <div>{ console.log('Rendered', this.props.intake)}</div>);
+            <form onSubmit={this.onSubmit}>
+                <input name='name' placeholder='Name' type='text' onChange={this.onNameChange} value={this.props.intake.name}/>
+                <input name='age' placeholder='Age' type='text' onChange={this.onAgeChange} value={this.props.intake.age}/>
+                <input name='school' placeholder='School' type='text' onChange={this.onSchoolChange} value={this.props.intake.school}/>
+                <button>Submit</button>
+            </form>
+        );
     }
 }
 
