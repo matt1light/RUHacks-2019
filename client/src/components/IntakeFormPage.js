80326de066b8baa4573b27ccc4cd26c4875d385b
dharshatharan <dharshatharan@gmail.com>
2019-05-18 02:32:13 -0400

    Component directory created
    Component folder was created and IntakeFormPage.js was created.

---
+++ client/src/components/IntakeFormPage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class IntakeFormPage extends Component {
+    state = {  }
+    render() { 
+        return (<div>HELLO</div>);
+    }
+}
+ 
+export default IntakeFormPage;
