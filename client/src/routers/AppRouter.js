0031937e3861b371a3fad2f91c00a1231f8f0895
matt1light <matt1light@gmail.com>
2019-05-18 11:36:43 -0400

    Add routes for homepage and intakeform

--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch} from 'react-router-dom';
+import IntakeFormPage from '../components/IntakeFormPage';
+import HomePage from '../components/HomePage'
 
 const AppRouter = () => (
   <BrowserRouter>
     <div>
       <Switch>
-        <Route path="/" component={<div>Welcome to WinWin</div>} exact={true} />
-        <Route path="/studentform" component={<div>student form</div>} />
-        <Route path="/matches/:id" component={<div>Matches</div>} />
-        <Route component={} />
+        <Route path="/" component={HomePage} exact={true} />
+        <Route path="/intakeform" component={IntakeFormPage} />
       </Switch>
     </div>
   </BrowserRouter>
