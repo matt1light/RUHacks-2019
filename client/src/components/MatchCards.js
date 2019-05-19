53c846d43ca0efbd017bf1e48b7da53d080dd9c9
Julia <julia.st.jean@winwinhomesharing.com>
2019-05-19 01:53:34 -0400

    Sample get from firestore

--- client/src/components/MatchCards.js
+++ client/src/components/MatchCards.js
@@ -3,17 +3,28 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid'
-
+import firestore, {withFirebase} from '../firebase';
+import { firebase } from 'react-redux-firebase';
 const cardStyle = {
     'borderStyle': 'dashed',
     'borderColor': 'rgba(0,0,0,0.3)',
 }
 
 
-class MatchCard extends Component{
+class MatchCardBase extends Component{
+    constructor(props){
+        super(props);
+        this.matches = ['03acb006-4eee-4483-b879-e40d183f965f'];
+    }
     clickMatch= () =>{
-        const button = document.getElementById('matchButton')
-        button.innerText= 'Matched :)'
+        const button = document.getElementById('matchButton');
+       this.props.firebase.collection("seniors").doc(this.matches[0]).get().then(function(querySnapshot) {
+           const doc = querySnapshot;
+           const name = doc.data()['name'];
+           console.log(doc.data()['name']);
+           button.innerText= name;
+       });
+
     };
 
     render(){
@@ -50,28 +61,6 @@ class MatchCard extends Component{
                         <button type="button" id ='matchButton' onClick = {this.clickMatch}>Match!</button>
                     </div>
                 </Card>
-                    <Card style={cardStyle}>
-                        <CardMedia
-                            component="img"
-                            alt="Avatar"
-                            height="140"
-                            image="https://randomuser.me/api/portraits/men/50.jpg"
-                            title="Avatar"
-                            style = {{'objectFit': 'contain'}}
-                        />
-                        <CardContent>
-                            <h2> first_name last_name</h2>
-                            <p> Age: <br/>
-                                City: <br/>
-                                Closest School: <br/>
-                                Tasks: <br/>
-                                Match: % <br/>
-                            </p>
-                        </CardContent>
-                        <div align = 'center'>
-                            <button type="button" id ='matchButton' onClick = {this.clickMatch}>Match!</button>
-                        </div>
-                    </Card>
                 </Grid>
             </div>
 
@@ -79,5 +68,5 @@ class MatchCard extends Component{
     }
 }
 
-
+const MatchCard = withFirebase(MatchCardBase);
 export default MatchCard;
