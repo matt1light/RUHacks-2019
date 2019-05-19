b27965983d22c588f8d8e764ebf30033597b67e1
MorganJSmith <Morgan.J.Smith@outlook.com>
2019-05-19 05:26:09 -0400

    Changed actions, app, and intakeformpage to be typescript
    Signed-off-by: MorganJSmith <Morgan.J.Smith@outlook.com>

--- client/src/actions/intake.js
+++
@@ -1,6 +0,0 @@
-export const updateForm = (formFields) => {
-    return {
-        type: 'UPDATE_FORM',
-        formFields
-    }
-}
---
+++ client/src/actions/intake.ts
@@ -0,0 +1,38 @@
+export interface IformFields{
+    id?: string,
+    name?: string,
+    email?: string,
+    class?: string,
+    age?: number,
+    tasks?: {
+        
+                bathroom?: boolean,
+                cook?: boolean,
+                dishes?: boolean,
+                drive?: boolean,
+                driveway?: boolean,
+                feed_pets?: boolean,
+                groceries?: boolean,
+                laundry?: boolean,
+                mop?: boolean,
+                mow_lawn?: boolean,
+                plants?: boolean,
+                trash?: boolean,
+                vacuum?: boolean,
+                walk_pets?: boolean,
+    },
+        
+        
+    city?: string,
+    school?: string,
+}
+
+export interface IupdateForm{
+  type: string,
+  formFields: IformFields,
+}
+
+export const updateForm = (formFields: IformFields):IupdateForm => ({
+  type: 'UPDATE_FORM',
+  formFields,
+});
