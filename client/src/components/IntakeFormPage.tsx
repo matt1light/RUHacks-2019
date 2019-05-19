5c025f101e49412ce450e7f91c8e7186ff48da62
dharshatharan <dharshatharan@gmail.com>
2019-05-19 08:01:10 -0400

    Added styles to form page

--- client/src/components/IntakeFormPage.tsx
+++ client/src/components/IntakeFormPage.tsx
@@ -6,6 +6,7 @@ import firestore, {withFirebase} from '../firebase';
 import {firebase} from 'react-redux-firebase';
 import {Redirect} from 'react-router';
 const uuidv4 = require('uuid/v4');
+
 const University = {
     Carleton: 'Carleton University',
     Ottawa: 'University of Ottawa',
@@ -101,38 +102,46 @@ export class IntakeFormPage extends Component<IntakeFormPageProps,IntakeFormPage
 
     render() {
         return (
-            <form onSubmit={this.onSubmit} id="intake">
-                <input name='name' placeholder='Name' type='text' maxLength={30} defaultValue="" onChange={this.onNameChange} value={this.props.intake_state.name}/><br/>
-                <input name='email' placeholder='Email' type='email' maxLength={30} defaultValue="" onChange={this.onEmailChange} value={this.props.intake_state.email}/><br/>
-                <input name='age' placeholder='Age' type='number' min={18} max={40} onChange={this.onAgeChange} value={this.props.intake_state.age}/><br/>
-                <select  form = "intake" onChange={this.onSchoolChange} value={this.props.intake_state.school}>
-                    <option disabled selected value="">Select University</option>
-                    <option value={University.Carleton}>Carleton University</option>
-                    <option value={University.Ottawa}>University of Ottawa</option>
-                    <option value={University.Toronto}>University of Toronto</option>
-                    <option value={University.Ryerson}>Ryerson University</option>
-                    <option value={University.York}>York University</option>
-                    <option value={University.Queens}>Queens University</option>
-                    <option value={University.Waterloo}>University of Waterloo</option>
-                </select><br/>
-                <input type="checkbox" name="vaccum" value="vacuum" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.vacuum}/> vacuum<br/>
-                <input type="checkbox" name="trash" value="trash" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.trash}/> trash<br/>
-                <input type="checkbox" name="bathroom" value="bathroom" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.bathroom}/> bathroom<br/>
-                <input type="checkbox" name="mop" value="mop" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.mop}/> mop<br/>
-                <input type="checkbox" name="dishes" value="dishes" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.dishes}/> dishes<br/>
-                <input type="checkbox" name="cook" value="cook" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.cook}/> cook<br/>
-                <input type="checkbox" name="groceries" value="groceries" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.groceries}/> groceries<br/>
-                <input type="checkbox" name="drive" value="drive" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.drive}/> drive<br/>
-                <input type="checkbox" name="driveway" value="driveway" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.driveway}/> driveway<br/>
-                <input type="checkbox" name="feed_pets" value="feed_pets" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.feed_pets}/> feed_pets<br/>
-                <input type="checkbox" name="walk_pets" value="walk_pets" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.walk_pets}/> walking pets<br/>
-                <input type="checkbox" name="laundry" value="laundry" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.laundry}/> laundry<br/>
-                <input type="checkbox" name="mow_lawn" value="mow_lawn" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.mow_lawn}/> mow_lawn<br/>
-                <input type="checkbox" name="plants" value="plants" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.plants}/> plants<br/>
-                <button onClick={this.resetDefault}> Reset</button> <button type="submit">Submit</button>
-                {this.state.redirect && <Redirect push to={'/matches'}/>}
-                <p><Link to={"/matches"}> We link now</Link></p>
+                <div>
+                    <img src={require("../WIN-WIN-Logo-322x169.png")} alt="WinWinLogo" height="100"/>
+                    <h3>Please fill in you information</h3>
+                <form onSubmit={this.onSubmit} id="intake">
+                <div>
+                        <div><span>Name</span>:<input name='name' placeholder='Name' type='text' maxLength={30} defaultValue="" onChange={this.onNameChange} value={this.props.intake_state.name}/></div><br/>
+                        <div><span>Email</span>:<input name='email' placeholder='Email' type='email' maxLength={30} defaultValue="" onChange={this.onEmailChange} value={this.props.intake_state.email}/></div><br/>
+                        <div className='ageInput'><span>Age:</span><input name='age' placeholder='Age' type='number' min = {18} max = {40} onChange={this.onAgeChange} value={this.props.intake_state.age}/></div><br/>
+                        <div>University:<select  form = "intake_state" onChange={this.onSchoolChange} value={this.props.intake_state.school}>
+                            <option disabled selected value="">Select University</option>
+                            <option value={University.Carleton} >Carleton University</option>
+                            <option value={University.Ottawa}>University of Ottawa</option>
+                            <option value={University.Toronto}>University of Toronto</option>
+                            <option value={University.Ryerson}>Ryerson University</option>
+                            <option value={University.York}>York University</option>
+                            <option value={University.Queens}>Queens University</option>
+                            <option value={University.Waterloo}>University of Waterloo</option>
+                        </select></div><br/>
+                        </div>
+                        <div className="bigBox">
+
+                        <input type="checkbox" className="Box-align" name="vaccum" value="vacuum" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.vacuum}/> vacuum<br/>
+                        <input type="checkbox" className="Box-align" name="trash" value="trash" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.trash}/>trash<br/>
+                        <input type="checkbox" className="Box-align" name="bathroom" value="bathroom" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.bathroom}/> bathroom<br/>
+                        <input type="checkbox" className="Box-align" name="mop" value="mop" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.mop}/> mop<br/>
+                        <input type="checkbox" className="Box-align" name="dishes" value="dishes" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.dishes}/> dishes<br/>
+                        <input type="checkbox" className="Box-align" name="cook" value="cook" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.cook}/> cook<br/>
+                        <input type="checkbox" className="Box-align" name="groceries" value="groceries" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.groceries}/> groceries<br/>
+                        <input type="checkbox" className="Box-align" name="drive" value="drive" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.drive}/> drive<br/>
+                        <input type="checkbox" className="Box-align" name="driveway" value="driveway" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.driveway}/> driveway<br/>
+                        <input type="checkbox" className="Box-align" name="feed_pets" value="feed_pets" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.feed_pets}/> feed_pets<br/>
+                        <input type="checkbox" className="Box-align" name="walk_pets" value="walk_pets" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.walk_pets}/> walking pets<br/>
+                        <input type="checkbox" className="Box-align" name="laundry" value="laundry" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.laundry}/> laundry<br/>
+                        <input type="checkbox" className="Box-align" name="mow_lawn" value="mow_lawn" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.mow_lawn}/> mow_lawn<br/>
+                        <input type="checkbox" className="Box-align" name="plants" value="plants" onChange={this.modifyTask} checked={this.props.intake_state.tasks!.plants}/> plants<br/>
+                        <button onClick={this.resetDefault} > Reset </button>
+                        <button type = "submit" >Submit</button>
+                </div>
             </form>
+            </div>
 
         );
     }
